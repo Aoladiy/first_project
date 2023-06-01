428bd788c17e63974b6a2e81f38dbeca052e54ea
Aldar <ochirov-3@mail.ru>
2023-06-01 16:51:29 +0300

    connected bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,34 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-<nav>
-    <ul>
-        <li>
-            <a href="{{route('welcome')}}">
-                welcome page
-            </a>
-        </li>
-        <li>
-            <a href="{{route('about')}}">
-                about page
-            </a>
-        </li>
-        <li>
-            <a href="{{route('contacts')}}">
-                contacts page
-            </a>
-        </li>
-        <li>
-            <a href="{{route('cv')}}">
-                cv page
-            </a>
-        </li>
-    </ul>
-</nav>
-<div>
-@yield('content')
+<div class="container">
+    <div class="row">
+        <nav>
+            <ul>
+                <li>
+                    <a href="{{route('welcome')}}">
+                        welcome page
+                    </a>
+                </li>
+                <li>
+                    <a href="{{route('about')}}">
+                        about page
+                    </a>
+                </li>
+                <li>
+                    <a href="{{route('contacts')}}">
+                        contacts page
+                    </a>
+                </li>
+                <li>
+                    <a href="{{route('cv')}}">
+                        cv page
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    @yield('content')
 </div>
 </body>
 </html>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.main')
 @section('content')
-    @foreach($posts as $post)
-        <p>{{$post->title}}</p>
-    @endforeach
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Content</th>
+            <th scope="col">Likes</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($posts as $post)
+        <tr>
+            <th scope="row">{{$post->id}}</th>
+            <td>{{$post->title}}</td>
+            <td>{{$post->content}}</td>
+            <td>{{$post->likes}}</td>
+        </tr>
+        @endforeach
+        </tbody>
+    </table>
 @endsection
